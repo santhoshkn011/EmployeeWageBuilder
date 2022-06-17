931407b24e86b86bb1f150bdf2d514136799c245
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-17 09:32:55 +0530

    Added: EmployeeWageGreetings added.

---
+++ src/com/employeeWage/EmpWageGreetings.java
@@ -0,0 +1,7 @@
+package com.employeeWage;
+
+public class EmpWageGreetings {
+    public static void main(String args[]) {
+        System.out.println("Welcome to Bridgelabz Employee Wage Builder.");
+    }
+}
