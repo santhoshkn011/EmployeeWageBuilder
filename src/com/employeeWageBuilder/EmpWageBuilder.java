7fbc1a08921d02d457354e18e80c8cbbbdab9aa7
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-17 09:56:05 +0530

    Added: Employee Daily Wage added.

--- src/com/employeeWageBuilder/EmpWageBuilder.java
+++ src/com/employeeWageBuilder/EmpWageBuilder.java
@@ -7,14 +7,20 @@ public class EmpWageBuilder {
         System.out.println("Welcome to Bridgelabz Employee Wage Builder.");
 
         int IS_FUll_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHours = 0;
+        int empWage = 0;
         Random random = new Random();
         int empCheck = random.nextInt(2);
 
         if(empCheck == IS_FUll_TIME) {
             System.out.println("Employee is Present.");
+            empHours = 8;
         }
         else {
             System.out.println("Employee is Absent.");
         }
+        empWage = empHours*EMP_RATE_PER_HOUR;
+        System.out.println("The daily wage is " + empWage);
     }
 }
