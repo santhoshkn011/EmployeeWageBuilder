07297132cc01c157aa4d46d3c9c837827c698a3f
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-17 14:28:54 +0530

    Added: Calculated Total wage for a month.

--- src/com/employeeWageBuilder/EmpWageBuilder.java
+++ src/com/employeeWageBuilder/EmpWageBuilder.java
@@ -9,27 +9,38 @@ public class EmpWageBuilder {
 
         //Declaring variables
         int EMP_RATE_PER_HOUR = 20;
-        int empHours = 0;
-        int empWage = 0;
+        int empHours;
+        int empWage;
+        int totalWage = 0;
+        int workingDaysPerMonth = 20; //Working days per month is 20 days
         Random random = new Random();
-        int empCheck = random.nextInt(3);
 
-        //Using switch and case.
-        switch (empCheck) {
-            case 1:
-                System.out.println("Employee is Present.");
-                empHours = 8;
-                break;
-            case 2:
-                System.out.println("Employee worked for half day.");
-                empHours = 4;
-                break;
-            default:
-                System.out.println("Employee is Absent.");
-                break;
+
+        //Calculating the total wage per month using for loop.
+
+        for(int day=1; day<=workingDaysPerMonth; day++) {
+
+            int empCheck = random.nextInt(3);
+
+            switch (empCheck) {
+                case 1:
+                    System.out.printf("Day %d: Employee is Present.", day).println();
+                    empHours = 8;
+                    break;
+                case 2:
+                    System.out.printf("Day %d: Employee worked for half day.", day).println();
+                    empHours = 4;
+                    break;
+                default:
+                    System.out.printf("Day %d: Employee is Absent.", day).println();
+                    empHours=0;
+                    break;
+            }
+            empWage = empHours*EMP_RATE_PER_HOUR;
+            System.out.println("Earned: $" + empWage + "\n");
+            totalWage += empWage;
         }
 
-        empWage = empHours*EMP_RATE_PER_HOUR;
-        System.out.println("The daily wage is " + empWage);
+        System.out.println("The Total wage earned for this month is " + "$" +totalWage);
     }
 }
