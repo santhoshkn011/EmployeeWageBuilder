9f17122b529ad473f5212204f0f0019c1f0992e2
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-17 13:52:02 +0530

    Added: Solving the empWage using Switch Case Statement.

--- src/com/employeeWageBuilder/EmpWageBuilder.java
+++ src/com/employeeWageBuilder/EmpWageBuilder.java
@@ -4,26 +4,31 @@ import java.util.Random; //importing Random function
 public class EmpWageBuilder {
     public static void main(String args[]) {
 
+        //Welcome message
         System.out.println("Welcome to Bridgelabz Employee Wage Builder.");
 
-        int IS_FUll_TIME = 1;
+        //Declaring variables
         int EMP_RATE_PER_HOUR = 20;
         int empHours = 0;
         int empWage = 0;
         Random random = new Random();
         int empCheck = random.nextInt(3);
 
-        if(empCheck == IS_FUll_TIME) {
-            System.out.println("Employee is Present.");
-            empHours = 8;
+        //Using switch and case.
+        switch (empCheck) {
+            case 1:
+                System.out.println("Employee is Present.");
+                empHours = 8;
+                break;
+            case 2:
+                System.out.println("Employee worked for half day.");
+                empHours = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent.");
+                break;
         }
-        else if(empCheck == 2){
-            System.out.println("Employee worked for half day.");
-            empHours = 4;
-        }
-        else {
-            System.out.println("Employee is Absent.");
-        }
+
         empWage = empHours*EMP_RATE_PER_HOUR;
         System.out.println("The daily wage is " + empWage);
     }
