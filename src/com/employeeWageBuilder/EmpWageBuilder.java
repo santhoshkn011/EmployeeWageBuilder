57db7839fcc80f22444d6d4549cb01c5eea63113
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-23 16:54:54 +0530

    Modified: Modified employee wage program by using class method and variables.

--- src/com/employeeWageBuilder/EmpWageBuilder.java
+++ src/com/employeeWageBuilder/EmpWageBuilder.java
@@ -1,51 +1,63 @@
+/*
+Refactor the Code to write a Class Method to Compute Employee Wage.
+Using Class Method and Class Variables.
+*/
 package com.employeeWageBuilder;
 import java.util.Random; //importing Random function
 
 public class EmpWageBuilder {
+    //declaring variables
+    public static int empHours = 0, EMP_RATE_PER_HOUR = 20,empWage = 0, totalWage = 0, workingDaysPerMonth = 20, maxWorkingHours = 0;
+    //Full TIme Employee Method
+    public static void fullTimeEmployee(int day){
+        System.out.printf("Day %d: Employee is Present for full day.", day).println();
+        empHours = 8;
+    }
+    //Part Time Employee Method
+    public static void partTimeEmployee(int day){
+        System.out.printf("Day %d: Employee worked for half day.", day).println();
+        empHours = 4;
+    }
+    //Absent Employee Method
+    public static void absentEmployee(int day){
+        System.out.printf("Day %d: Employee is Absent.", day).println();
+        empHours = 0;
+    }
+    //Calculation of employee wage and working hours method
+    public static void empWageAndWorkingHours(){
+        empWage = empHours * EMP_RATE_PER_HOUR;
+        System.out.printf("Worked: %d hours, Earned: $%d", empHours, empWage).println("\n");
+        totalWage += empWage;
+        maxWorkingHours += empHours; //Max Working Hours is 100, loop terminates if it is above 100.
+    }
+    public static void totalWage(){
+        System.out.println("The Total wage earned for this month is " + "$" +totalWage);
+    }
+    public static void maxWorkingHours(){
+        System.out.println("Total hours worked: " + maxWorkingHours);
+    }
     public static void main(String args[]) {
-
         //Welcome message
         System.out.println("Welcome to Bridgelabz Employee Wage Builder. \n");
-
-        //Declaring variables
-        int EMP_RATE_PER_HOUR = 20;
-        int empHours = 0;
-        int empWage = 0;
-        int totalWage = 0;
-        int workingDaysPerMonth = 20; //Working days per month is 20 days
-        int maxWorkingHours = 0;
         Random random = new Random();
-
-
         //Calculating the total wage per month using loops
-        //Max Working hours should be 100.
-
             for (int day = 1; day<=workingDaysPerMonth && maxWorkingHours<100; day++) {
-
+                //Max Working hours should be 100.
                 int empCheck = random.nextInt(3);
-
                 switch (empCheck) {
                     case 1:
-                        System.out.printf("Day %d: Employee is Present for full day.", day).println();
-                        empHours = 8;
-                        //printf(): This method belongs to the PrintStream class.
+                        fullTimeEmployee(day); //invoking full time employee method
                         break;
                     case 2:
-                        System.out.printf("Day %d: Employee worked for half day.", day).println();
-                        empHours = 4;
+                        partTimeEmployee(day); //invoking part time
                         break;
                     default:
-                        System.out.printf("Day %d: Employee is Absent.", day).println();
-                        empHours = 0;
+                        absentEmployee(day); //invoking absent
                         break;
                 }
-                empWage = empHours * EMP_RATE_PER_HOUR;
-                System.out.printf("Worked: %d hours, Earned: $%d", empHours, empWage).println("\n");
-                totalWage += empWage;
-                maxWorkingHours += empHours; //Max Working Hours is 100, loop terminates if it is above 100.
+                empWageAndWorkingHours(); //Iterating total wage and max working hours method
             }
-        System.out.println("The Total wage earned for this month is " + "$" +totalWage);
-        System.out.println("Total hours worked: " + maxWorkingHours);
-
+            totalWage(); //invoking total wage
+            maxWorkingHours(); //invoking max hours worked in a month.
     }
 }
