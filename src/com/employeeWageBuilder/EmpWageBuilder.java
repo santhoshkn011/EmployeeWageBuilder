841f5f4db9169565e837eaf12075ebefb85d6c96
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-24 20:11:53 +0530

    Modified: Used Instance Variables and saved the Total wage.

--- src/com/employeeWageBuilder/EmpWageBuilder.java
+++ src/com/employeeWageBuilder/EmpWageBuilder.java
@@ -1,30 +1,30 @@
 /*
-Refactor the Code to write a Class Method to Compute Employee Wage.
-Using Class Method and Class Variables.
+Ability to save the Total Wage for Each Company -
+Note: You can Create EmpWageBuilder for each Company
+- Use Instance Variable instead of function parameters
 */
 package com.employeeWageBuilder;
 import java.util.Random; //importing Random function
 
 public class EmpWageBuilder {
-    //declaring variables
-    public static int empHours = 0;
-    //Full TIme Employee Method
-    public static void fullTimeEmployee(int day){
-        System.out.printf("Day %d: Employee is Present for full day.", day).println();
-        empHours = 8;
+    //Declaring instance variables
+    private final String comp;
+    private final int EMP_RATE_PER_HOUR;
+    private final int workingDaysPerMonth;
+    private final int maxWorkingHours;
+    private int totalEmpWage;
+    //Constructor for the class EmpWageBuilder
+    public EmpWageBuilder(String comp, int EMP_RATE_PER_HOUR, int workingDaysPerMonth, int maxWorkingHours){
+        this.comp = comp;
+        this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+        this.workingDaysPerMonth = workingDaysPerMonth;
+        this.maxWorkingHours = maxWorkingHours;
     }
-    //Part Time Employee Method
-    public static void partTimeEmployee(int day){
-        System.out.printf("Day %d: Employee worked for half day.", day).println();
-        empHours = 4;
-    }
-    //Absent Employee Method
-    public static void absentEmployee(int day){
-        System.out.printf("Day %d: Employee is Absent.", day).println();
-        empHours = 0;
-    }
-    public static void companyWage(String comp, int EMP_RATE_PER_HOUR, int workingDaysPerMonth, int maxWorkingHours ){
-        int empWage = 0, totalWage=0, workingHours = 0;
+    //Computation of company wage
+    public void companyWage(){
+        System.out.printf("%s Employee Wage details: ", comp).println();
+        //local variables
+        int empWage = 0, totalWage=0, workingHours = 0, empHours = 0;
         Random random = new Random();
         //Calculating the total wage per month using loops
         for (int day = 1; day<=workingDaysPerMonth && workingHours<maxWorkingHours; day++) {
@@ -32,13 +32,16 @@ public class EmpWageBuilder {
             int empCheck = random.nextInt(3);
             switch (empCheck) {
                 case 1:
-                    fullTimeEmployee(day); //invoking full time employee method
+                    System.out.printf("Day %d: Employee is Present for full day.", day).println();
+                    empHours = 8;
                     break;
                 case 2:
-                    partTimeEmployee(day); //invoking part time
+                    System.out.printf("Day %d: Employee worked for half day.", day).println();
+                    empHours = 4;
                     break;
                 default:
-                    absentEmployee(day); //invoking absent
+                    System.out.printf("Day %d: Employee is Absent.", day).println();
+                    empHours = 0;
                     break;
             }
             empWage = empHours * EMP_RATE_PER_HOUR;
@@ -46,24 +49,24 @@ public class EmpWageBuilder {
             totalWage += empWage;
             workingHours += empHours; //Max Working Hours is 100, loop terminates if it is above 100.
         }
-        totalWage(comp, totalWage); //invoking total wage
-        maxWorkingHours(comp, workingHours);//invoking max hours worked in a month.
+        totalEmpWage = workingHours*EMP_RATE_PER_HOUR;
+        System.out.printf("The Total wage earned by %s for this month: " + "$ " + totalWage, comp).println();
+        System.out.printf("Total hours worked for %s: " + workingHours, comp).println();
         System.out.println();
     }
-    //Calculation of employee wage and working hours method
-    public static void totalWage(String comp, int wage){
-        System.out.printf("The Total wage earned by %s for this month: " + "$ " + wage, comp).println();
+    //overriding the toString() method
+    public String toString(){
+        return "Total wage earned for the Company " + comp + " is: " + totalEmpWage;
     }
-    public static void maxWorkingHours(String comp, int workingHours){
-        System.out.printf("Total hours worked for %s: " + workingHours, comp).println();
-    }
-    //Starting of main method
+    //Starting of main method.
     public static void main(String args[]) {
         //Welcome message
-        System.out.println("Welcome to Bridgelabz Employee Wage Builder. \n");
-        System.out.println("Employee Wage Builder for Company(BridgeLabz): \n");
-        companyWage("BridgeLabz",20,20,100);
-        System.out.println("Employee Wage Builder for Company(Toyota): \n");
-        companyWage("Toyota",30,25,120);
+        System.out.println("Welcome to Employee Wage Builder. \n");
+        EmpWageBuilder bridgelabz = new EmpWageBuilder("Bridgelabz", 20, 20, 100);
+        bridgelabz.companyWage();
+        EmpWageBuilder toyota = new EmpWageBuilder("Toyota", 30, 25, 120);
+        toyota.companyWage();
+        System.out.println(bridgelabz);
+        System.out.println(toyota);
     }
 }
