6b43867c8aa520d8ce6ee23176e63dcc5a29c3a0
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-25 05:32:26 +0530

    Added: Printing out the total wage of the company entered.

--- src/com/employeeWageBuilder/EmpWageBuilder.java
+++ src/com/employeeWageBuilder/EmpWageBuilder.java
@@ -1,26 +1,34 @@
 /*
-Store the Daily Wage along with the Total Wage
+Ability to get the Total Wage when queried by Company - Create Method in
 The ArrayList class is a resizable array, which can be found in the java. util package.
 The difference between a built-in array and an ArrayList in Java, is that the size of an array cannot be modified (if you want to add or remove elements to/from an array, you have to create a new one).
 An Interface in Java programming language is defined as an abstract type used to specify the behavior of a class.
 An interface in Java is a blueprint of a class. A Java interface contains static constants and abstract methods. The interface in Java is a mechanism to achieve abstraction.
 Data abstraction is the process of hiding certain details and showing only essential information to the user.
+
+Java HashMap class implements the Map interface which allows us to store key and value pair, where keys should be unique.
 */
 package com.employeeWageBuilder;
 import java.util.Random; //importing Random function
 import java.util.ArrayList; //importing ArrayList class
 import java.util.HashMap; //Importing HashMap
+import java.util.Scanner;
 //implementing interface
 interface IEmpWageBuilder {
     public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs);
     public void companyWage();
+
+    public static int getTotalEmpWage(String companyName) {
+        return 0;
+    }
 }
 
 class EmpWageBuilder implements IEmpWageBuilder{
     // instance variables
     int noOfCompanies, index;
     ArrayList<CompanyEmpWage> companies; //ArrayList declaration
-    HashMap<String, Integer> fullMonthWage; //HashMap Declaration, String = Company name, Integer = Full month wage.
+    //company name is mapped with total employee wage
+    static HashMap<String, Integer> fullMonthWage; //HashMap Declaration, String = Company name, Integer = Full month wage.
     //Constructor for the class EmpWageBuilder
     public EmpWageBuilder() {
         companies = new ArrayList<>(); //Creating Array list
@@ -73,15 +81,13 @@ class EmpWageBuilder implements IEmpWageBuilder{
             System.out.println(company); //overriding the toString() method
         }
     }
-    void printTotalEmpWages(){
-        System.out.println("The Companies and the full Month wage details of the Employee:");
-        for (String companyName : fullMonthWage.keySet()) //It is used to return a set view of the keys contained in this map.
-        {
-            System.out.println(companyName + ": " + fullMonthWage.get(companyName)); //Get method to get the value of key value.
-        }
+    public static int getTotalEmpWage(String companyName) {
+        return fullMonthWage.get(companyName);
     }
+
     //Starting of main method.
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         //Welcome message
         System.out.println("Welcome to Employee Wage Builder. \n");
         EmpWageBuilder emp = new EmpWageBuilder(); //creating an object and declaring number of companies = 3
@@ -89,6 +95,9 @@ class EmpWageBuilder implements IEmpWageBuilder{
         emp.addCompany("Toyota", 30, 22, 120);
         emp.addCompany("TCS", 25, 25, 115);
         emp.companyWage(); // Daily Wage along with the Total Wage
-        emp.printTotalEmpWages(); // Total Wage of the month of all companies.
+        System.out.println("Type the Company Name");
+        String comp = sc.nextLine();
+        int totalWage = EmpWageBuilder.getTotalEmpWage(comp);
+        System.out.println("The Total Wage for " + comp + " Company is " + totalWage + " for this month.");
     }
 }
